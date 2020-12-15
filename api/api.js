af28d9fd29e02d091fed3c4b4c553bdec6063c29
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-12-15 17:42:44 +0100

    arreglando error

--- api/api.js
+++ api/api.js
@@ -155,7 +155,7 @@ try {
 					let total = elegirNSitios("monumentos", Math.floor(Math.random() * monumentos.length))
 
 					let cad = "Según tu ubicación el lugar más cercano al que puedes ir es: "
-					+ total[0].json()
+					+ total.length
 
 					return res.send({fulfillmentText: cad})
 				}
