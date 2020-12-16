c46451058e5cac70ead8f035656702d4b4edbd05
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-12-16 18:07:39 +0100

    incluyendo archivos

--- api/api.js
+++ api/api.js
@@ -62,129 +62,17 @@ async function getPrecio(tipo) {
 /******************************************************************************/
 /******************************************************************************/
 /******************************************************************************/
-
 module.exports = (req, res) => {
-
-try {
-	let query = req.body.queryResult.intent.displayName
+	try {
+		let query = req.body.queryResult.intent.displayName
 /******************************************************************************/
 /******************************************************************************/
 /******************************************************************************/
-	switch (query) {
-		case "consultaSiguienteSitio":
-			try {
-
-				let cad = "El siguiente sitio a visitar de la ruta es: " + ruta[0]
-
-				return res.send({fulfillmentText: cad})
-			}
-			catch (err) {
-				return res.send({fulfillmentText: "Ha habido algun error: " + err})
-			}
-		break;
-/******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
-		case "consultaLugaresVisitados":
-			try {
-				let sitios = req.body.queryResult.outputContexts[0].parameters.CantidadSitios
-				let tipo = req.body.queryResult.outputContexts[0].parameters.LugaresInteres
-				let nSitios = parseInt(sitios)
-				let total = elegirNSitios(tipo, nSitios)
-				let nSitiosVisitados = generarEntero(1, total.length)
-
-				let cad = "Por ahora se han visitado los lugares: \n"
-
-				for (let i=0; i < nSitiosVisitados; i++) {
-					if(i+2 == nSitiosVisitados)
-						cad += total[i] + " y ";
-					else
-						cad += total[i] + ", ";
-				}
-
-				return res.send({fulfillmentText: cad})
-			}
-			catch (err) {
-				return res.send({fulfillmentText: "Ha habido algun error: " + err})
-			}
-		break;
-/******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
-		case "consultaKilometrosPorRecorrer":
-			try {
-				let MARGEN = 20
-				let km = generarEntero(1, MARGEN)
-				let kmFaltantes = generarEntero(1, MARGEN)
-
-				let cad = "Llevas andados " + km + " km y quedan todavia "
-				+ kmFaltantes + " km por andar"
-
-				return res.send({fulfillmentText: cad})
-			}
-			catch (err) {
-				return res.send({fulfillmentText: "Ha habido algun error: " + err})
-			}
-		break;
-/******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
-		case "consultaLugaresPorVisitar":
-			try {
-				let sitios = req.body.queryResult.outputContexts[0].parameters.CantidadSitios
-				let tipo = req.body.queryResult.outputContexts[0].parameters.LugaresInteres
-				let nSitios = parseInt(sitios)
-				let total = elegirNSitios(tipo, nSitios)
-				let nSitiosVisitados = generarEntero(1, total.length)
-
-				let cad = "Quedan por visitar: \n"
-
-				for (let i=0; i < nSitiosVisitados; i++) {
-					if(i+2 == nSitiosVisitados)
-						cad += total[i] + " y ";
-					else
-						cad += total[i] + ", ";
-				}
-
-				return res.send({fulfillmentText: cad})
-			}
-			catch (err) {
-				return res.send({fulfillmentText: "Ha habido algun error: " + err})
-			}
-		break;
-/******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
-		case "generarRecorrido":
-			try {
-				let sitios = req.body.queryResult.outputContexts[0].parameters.CantidadSitios
-				let tipo = req.body.queryResult.outputContexts[0].parameters.LugaresInteres
-				let nSitios = parseInt(sitios)
-				let total = elegirNSitios(tipo, nSitios)
-
-				let cad = "La ruta creada es: \n"
-				ruta = []
-				total.forEach((elemento) => {
-					cad += elemento + ", "
-					ruta.push(elemento)
-				})
-
-				return res.send({fulfillmentText: cad})
-			}
-			catch (err) {
-				return res.send({fulfillmentText: "Ha habido algun error: " + err})
-			}
-			break;
-/******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
-			case "recomendacionLugarCercano":
+		switch (query) {
+			case "consultaSiguienteSitio":
 				try {
-					let rand = generarEntero(1, monumentos.length);
-					let total = elegirNSitios("monumentos", rand)
 
-					let cad = "Segun tu ubicación el lugar más cercano al que puedes ir es: "
-					+ total[0]
+					let cad = "El siguiente sitio a visitar de la ruta es: " + ruta[0]
 
 					return res.send({fulfillmentText: cad})
 				}
@@ -195,26 +83,136 @@ try {
 /******************************************************************************/
 /******************************************************************************/
 /******************************************************************************/
-		case "comprarEntradas":
-			try {
-				(async () => {
-					let precio = 0;
-					let tipoTicket = req.body.queryResult.outputContexts[0].parameters.TipoCiudadano
+			case "consultaLugaresVisitados":
+				try {
+					let sitios = req.body.queryResult.outputContexts[0].parameters.CantidadSitios
+					let tipo = req.body.queryResult.outputContexts[0].parameters.LugaresInteres
+					let nSitios = parseInt(sitios)
+					let total = elegirNSitios(tipo, nSitios)
+					let nSitiosVisitados = generarEntero(1, total.length)
 
+					let cad = "Por ahora se han visitado los lugares: \n"
 
-					entradas.getEntradas().then(entrad => {
-							for(let i=0; i < entrad.length; i++) {
-								if(entrad[i].tipo == tipoTicket) {
-									precio = entrad[i].precio
-									return res.send({fulfillmentText: "El precio para " + tipoTicket + " es de " + precio + "€"});
+					for (let i=0; i < nSitiosVisitados; i++) {
+						if(i+2 == nSitiosVisitados)
+							cad += total[i] + " y ";
+						else
+							cad += total[i] + ", ";
+					}
+
+					return res.send({fulfillmentText: cad})
+				}
+				catch (err) {
+					return res.send({fulfillmentText: "Ha habido algun error: " + err})
+				}
+			break;
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+			case "consultaKilometrosPorRecorrer":
+				try {
+					let MARGEN = 20
+					let km = generarEntero(1, MARGEN)
+					let kmFaltantes = generarEntero(1, MARGEN)
+
+					let cad = "Llevas andados " + km + " km y quedan todavia "
+					+ kmFaltantes + " km por andar"
+
+					return res.send({fulfillmentText: cad})
+				}
+				catch (err) {
+					return res.send({fulfillmentText: "Ha habido algun error: " + err})
+				}
+			break;
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+			case "consultaLugaresPorVisitar":
+				try {
+					let sitios = req.body.queryResult.outputContexts[0].parameters.CantidadSitios
+					let tipo = req.body.queryResult.outputContexts[0].parameters.LugaresInteres
+					let nSitios = parseInt(sitios)
+					let total = elegirNSitios(tipo, nSitios)
+					let nSitiosVisitados = generarEntero(1, total.length)
+
+					let cad = "Quedan por visitar: \n"
+
+					for (let i=0; i < nSitiosVisitados; i++) {
+						if(i+2 == nSitiosVisitados)
+							cad += total[i] + " y ";
+						else
+							cad += total[i] + ", ";
+					}
+
+					return res.send({fulfillmentText: cad})
+				}
+				catch (err) {
+					return res.send({fulfillmentText: "Ha habido algun error: " + err})
+				}
+			break;
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+			case "generarRecorrido":
+				try {
+					let sitios = req.body.queryResult.outputContexts[0].parameters.CantidadSitios
+					let tipo = req.body.queryResult.outputContexts[0].parameters.LugaresInteres
+					let nSitios = parseInt(sitios)
+					let total = elegirNSitios(tipo, nSitios)
+
+					let cad = "La ruta creada es: \n"
+					ruta = []
+					total.forEach((elemento) => {
+						cad += elemento + ", "
+						ruta.push(elemento)
+					})
+
+					return res.send({fulfillmentText: cad})
+				}
+				catch (err) {
+					return res.send({fulfillmentText: "Ha habido algun error: " + err})
+				}
+			break;
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+				case "recomendacionLugarCercano":
+					try {
+						let rand = generarEntero(1, monumentos.length);
+						let total = elegirNSitios("monumentos", rand)
+
+						let cad = "Segun tu ubicación el lugar más cercano al que puedes ir es: "
+						+ total[0]
+
+						return res.send({fulfillmentText: cad})
+					}
+					catch (err) {
+						return res.send({fulfillmentText: "Ha habido algun error: " + err})
+					}
+				break;
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+			case "comprarEntradas":
+				try {
+					(async () => {
+						let precio = 0;
+						let tipoTicket = req.body.queryResult.outputContexts[0].parameters.TipoCiudadano
+
+						entradas.getEntradas().then(entrad => {
+								for(let i=0; i < entrad.length; i++) {
+									if(entrad[i].tipo == tipoTicket) {
+										precio = entrad[i].precio
+										return res.send({fulfillmentText: "El precio para "
+										+ tipoTicket + " es de " + precio + "€"});
+									}
 								}
-							}
-					});
-				})();
-			}
-			catch (err) {
-				return res.send({fulfillmentText: "Error: " + err})
-			}
+						});
+					})();
+				}
+				catch (err) {
+					return res.send({fulfillmentText: "Error: " + err})
+				}
 			break;
 /******************************************************************************/
 /******************************************************************************/
@@ -231,17 +229,16 @@ try {
 						if(enfermedad[i][0] == sintoma)
 							return res.send({fulfillmentText: "Consejo: " + enfermedad[i][1]})
 					}
-
 				}
 				catch (err) {
 					return res.send({fulfillmentText: "Error: " + err})
 				}
-				break;
+			break;
 /******************************************************************************/
 /******************************************************************************/
 /******************************************************************************/
-		}
-	}
+		} // Fin switch
+	} // Fin try
 	catch (err) {
 		return res.send({fulfillmentText: "Ha habido algun error: " + err})
 	}
